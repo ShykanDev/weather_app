f212c7f27417b864e45047202267d17d407b09bc
ShknDev <awscodeapp@gmail.com>
2024-07-23 19:00:38 -0600

    Fixed the public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/WeathyApp.github.io'
+  publicPath: '/weather_app'
 })
